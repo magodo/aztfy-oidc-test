3e7665645f479d48dd6134e724476ed8b8818e39
magodo <wztdyl@sina.com>
2024-08-11 21:38:30 +0800

    update

--- oauth2/main.go
+++ oauth2/main.go
@@ -55,7 +55,6 @@ func main() {
 
 	c := oauth2.NewClient(ctx, ts)
 	resp, err := c.Get(fmt.Sprintf("https://management.azure.com/subscriptions/%s/resourcegroups/aztfy?api-version=2020-06-01", *subId))
-	log.Println(resp)
 	if resp != nil {
 		b, err := io.ReadAll(resp.Body)
 		if err != nil {
